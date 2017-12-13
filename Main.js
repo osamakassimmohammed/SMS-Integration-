8061345a60b243a69b84acf70e7339e618b30973
Osama Kassim <Osema.kassim@gmail.com>
2017-12-13 21:19:49 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,16 +1,16 @@
 
 function SMSTOUSERS(){
-var accountSid = 'AC79a898de055f4848e78dd51a9fe36516'; // Your Account SID from www.twilio.com/console
-var authToken = 'a5465b9d27b7f2f262591428e83fdaed';   // Your Auth Token from www.twilio.com/console
+var accountSid = ''; // Your Account SID from www.twilio.com/console
+var authToken = '';   // Your Auth Token from www.twilio.com/console
 
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 
 client.messages.create({
-    body: 'Hello Mano This is Osama (:',
-    //to: '+966557730782',
-	to: '+966592688256',
-    from: '+18505839282 ',
+    body: 'Hello  This is Osama (:',
+    //to: '+',
+	to: '+',
+    from: '+ ',
 })
 .then((message) => console.log(message.sid));
 
